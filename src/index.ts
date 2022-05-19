24fdbb15a3a70226e97d239c15bfd5d90c51c6d5
Steve Smodish <smodish@hotmail.com>
2022-05-18 22:40:15 -0400

    the 'bad code' version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello')
+import fs from 'fs'
+
+// Load
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  // Parse
+  .split('\n')
+  .map((match: string): string[] => match.split(','))
+
+// Analyze
+let manUnitedWins = 0
+for (const match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++
+  }
+  if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++
+  }
+}
+
+// Report
+console.log(`Man United won ${manUnitedWins} games`)
