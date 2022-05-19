ab512610fe265d7a624b825a27130a77cef24417
Steve Smodish <smodish@hotmail.com>
2022-05-19 14:23:54 -0400

    REFACTOR Abstract Class with Generics

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { CsvFileReader } from './CsvFileReader'
+import { MatchReader } from './MatchReader'
 import { MatchResult } from './MatchResult'
 
 // Load
-const reader = new CsvFileReader('football.csv')
+const reader = new MatchReader('football.csv')
 reader.read()
 
 // Analyze
